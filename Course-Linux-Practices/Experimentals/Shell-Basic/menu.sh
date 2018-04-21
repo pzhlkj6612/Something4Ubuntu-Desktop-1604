afdbeda1903e3c80b0d9d54ba4277e673a9e6494
Mozi <pzhlkj6612@126.com>
2018-04-22 00:07:32 +0800

    Create menu.sh

---
+++ Course-Linux-Practices/Experimentals/Shell-Basic/menu.sh
@@ -0,0 +1,200 @@
+#! /bin/bash
+
+
+workSpace=$(pwd)
+folderName=Kimi_no_Na_wa
+file1Name=file1
+file2Name=file2
+
+
+function Cycle(){
+	while true
+	do
+		DrawMenu
+		InputProc
+	done
+}
+
+
+function InputProc(){
+	echo -n "Enter your choice: "
+	read input
+
+	case $input in
+		'A'|'a')
+			c_A
+			EndProc
+			;;
+		'B'|'b')
+			c_B
+			EndProc
+			;;
+		'C'|'c')
+			c_C
+			EndProc
+			;;
+		'D'|'d')
+			c_D
+			EndProc
+			;;
+		'E'|'e')
+			c_E
+			EndProc
+			;;
+		'Q'|'q')
+			c_Q
+			EndProc
+			;;
+		*)
+			echo 'Try again!'
+			read
+	esac
+}
+
+
+function EndProc(){
+	echo
+	echo
+	echo -n "Press any key to continue..."
+	read
+}
+
+
+function InitKeyFiles(){
+	touch ~/$file1Name
+	man man | head -n 10 > ~/$file1Name
+
+	touch ~/$file2Name
+	man ls | head -n 10 > ~/$file2Name
+}
+
+
+function c_A(){
+	echo
+	echo
+	echo " ****** Create subfolder and copy files ******"
+	echo
+
+	mkdir ~/$folderName 
+
+	chmod 777 ~/$folderName
+	chmod g-w,o-w ~/$folderName
+
+	if [ $(find ~ -type d -name "$folderName" | wc -l) -eq 1 ]
+		then
+			cp ~/$file1Name ~/$folderName/
+			cp ~/$file2Name ~/$folderName/
+
+			echo "Content of \"$file1Name\" in folder \"~/$folderName/\":"
+			cat ~/$folderName/$file1Name
+
+			echo
+			echo "Content of \"$file2Name\" in folder \"~/$folderName/\":"
+			cat ~/$folderName/$file2Name
+
+			:>~/$folderName/$file1Name
+			:>~/$folderName/$file2Name
+	fi
+}
+
+
+function c_B(){
+	echo
+	echo
+	echo " ****** Permission test ******"
+	echo
+
+	foundFilePath=$(find /etc/ -maxdepth 1 -type f | head -n 1)
+	ls -l $foundFilePath | grep 'rwx......' 1>/dev/null
+	if [ $? -eq 1 ]
+		then
+			echo "I don't have \"rwx\" permission with \"$foundFilePath\"."
+		else
+			echo "I have \"rwx\" permission with \"$foundFilePath!\"."
+	fi
+}
+
+
+function c_C(){
+	file1Name_New=file1_new
+	file2Name_New=file2_new
+
+	echo
+	echo
+	echo " ****** Characters convert ******"
+	echo
+	echo "Translated content of \"file1\":"
+	cat ~/$file1Name | tr '[a-z]' '[A-Z]' > $workSpace/$file1Name_New && cat $workSpace/$file1Name_New
+
+	echo
+	echo
+	echo "Translated content of \"file2\":"
+	cat ~/$file2Name | tr '[a-z]' '[A-Z]' > $workSpace/$file2Name_New && cat $workSpace/$file2Name_New
+
+	# Clear
+	rm $workSpace/$file1Name_New
+	rm $workSpace/$file2Name_New
+}
+
+
+function c_D(){
+	echo
+	echo
+	echo " ****** Do counting & show env ******"
+	echo
+	echo "Count of folders in \"/dev/\": $(ls -l /dev/ | grep '^d.*' | wc -l)"
+	echo "Count of symbolic link files in \"/dev/\": $(ls -l /dev/ | grep '^l.*' | wc -l)"
+
+	echo
+	foundFilePath=$(find /etc/ -maxdepth 1 -type f | head -n 1)
+	echo "Count of empty lines in \"$foundFilePath\": $(cat $foundFilePath | grep '^$' | wc -l)"
+
+	echo
+	echo "All environment variables here:"
+	env
+}
+
+
+function c_E(){
+	echo
+	echo
+	echo " ****** Find something ******"
+	echo
+	find '/dev/' -name 's????' -exec 'file' {} \;
+}
+
+
+function DrawMenu(){
+	clear
+	echo -e $"\t\tUSER: $(whoami)\tHOST: $(hostname)\tDATE: $(date)"
+	echo -e $"\t\t--------------------------------------------------------------------"
+	echo
+	echo -e $"\t\t\tA. Create subfolder and copy files"
+	echo -e $"\t\t\tB. Permission test"
+	echo -e $"\t\t\tC. Characters convert"
+	echo -e $"\t\t\tD. Do counting & show env"
+	echo -e $"\t\t\tE. Find something"
+	echo -e $"\t\t\tQ. Say goodbye"
+	echo
+	echo -e $"\t\t--------------------------------------------------------------------"
+	echo
+}
+
+
+function c_Q(){
+	#Clean
+	rm ~/$file1Name
+	rm ~/$file2Name
+	rm -rdf ~/$folderName
+	
+	clear
+	echo "Goodbye!"
+	echo
+	exit
+}
+
+
+#public static int mian(){
+	InitKeyFiles
+	Cycle
+#}
